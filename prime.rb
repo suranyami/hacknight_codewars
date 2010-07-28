6243350803c04be5ffd113eeb5f1a70b2e05241f
David Parry <david.parry@suranyami.com>
2010-07-28 19:36:41 +1000

    prime answers.

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
+require 'mathn'
+
 class Prime
   def self.find(n)
+    p = Prime.new
+    result = p.succ
+    (n-1).times do
+      result = p.succ
+    end
+    result
   end
 end
+
+puts "Found: #{Prime.find(100)}"
--- prime_spec.rb
+++ prime_spec.rb
@@ -1,6 +1,18 @@
 require 'prime'
 
 describe "10,001st prime" do
+  it "first should be 2" do
+    Prime.find(1).should == 2
+  end
+  
+  it "second should be 3" do
+    Prime.find(2).should == 3
+  end
+  
+  it "does something" do
+    (2 * 2 == 4).should be true
+  end
+  
   it "should be 6 digits and end in 43" do
     prime_10001 = Prime.find(10001).to_s
     prime_10001.size.should == 6
