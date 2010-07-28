cb2c512f76c151e4d0776fed3a3c19e9b128e68e
David Parry <david.parry@suranyami.com>
2010-07-28 21:48:47 +1000

    Added t9 map

--- decoder.rb
+++ decoder.rb
@@ -1,5 +1,16 @@
-
 class Decoder
+  t9_map = {
+    2 => %w(abc),
+    3 => %w(def),
+    4 => %w(ghi),
+    5 => %w(jkl),
+    6 => %w(mno),
+    7 => %w(pqrs),
+    8 => %w(tuv),
+    9 => %w(wxyz),
+    0 => %w(+)
+  }
+  
   def encode(message)
   end
 
